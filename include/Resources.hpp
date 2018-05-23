8cb1195945a674f11fed1f547daebaade27ce724
Keksi <juhanakekoni@yahoo.com>
2018-05-23 23:04:52 +0300

    Resources: Add getFont()
    Works pretty much like getTexture, except caches sf:Text

--- include/Resources.hpp
+++ include/Resources.hpp
@@ -11,11 +11,14 @@ class Resources {
         Resources();
 
         sf::Texture const& getTexture(std::string const& filename);
+        sf::Font const& getFont(std::string const& filename);
 
     private:
-
         std::string _texture_path;
+        std::string _font_path;
+
         std::unordered_map<std::string, sf::Texture> _textures;
+        std::unordered_map<std::string, sf::Font> _fonts;
 };
 
 #endif // EXO_RESOURCES_HPP
--- src/Resources.cpp
+++ src/Resources.cpp
@@ -2,11 +2,18 @@
 #include "Resources.hpp"
 
 Resources::Resources() :
-_texture_path("tex/"), _textures() {}
+_texture_path("tex/"), _font_path("font/"), _textures(), _fonts() {}
 
 sf::Texture const& Resources::getTexture(std::string const& filename) {
     if (_textures.find(filename) == _textures.end()) {
         _textures[filename].loadFromFile(_texture_path + filename);
     }
     return _textures.at(filename);
+}
+
+sf::Font const& Resources::getFont(std::string const& filename) {
+    if (_fonts.find(filename) == _fonts.end()) {
+        _fonts[filename].loadFromFile(_texture_path + filename);
+    }
+    return _fonts.at(filename);
 }
