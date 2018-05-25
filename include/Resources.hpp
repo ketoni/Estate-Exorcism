65b2a8c9ce1098f9a23c63ca23356d1f7cd8d756
Teo <teo.haapajarvi@aalto.fi>
2018-05-25 20:11:50 +0300

    Added functions for playing sounds and music

--- include/Resources.hpp
+++ include/Resources.hpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 class Resources {
 
@@ -12,6 +13,8 @@ class Resources {
 
         sf::Texture const& getTexture(std::string const& filename);
         sf::Font const& getFont(std::string const& filename);
+        sf::Sound& getSound(std::string const& filename);
+        sf::Music& getMusic(std::string const& filename);
 
         sf::Shader* allocateShader(std::string const& filename, sf::Shader::Type type);
 
@@ -19,9 +22,14 @@ class Resources {
         std::string _texture_path;
         std::string _font_path;
         std::string _shader_path;
+        std::string _sound_path;
+        std::string _music_path;
 
         std::unordered_map<std::string, sf::Texture> _textures;
         std::unordered_map<std::string, sf::Font> _fonts;
+        std::unordered_map<std::string, sf::Sound> _sounds;
+        std::unordered_map<std::string, sf::SoundBuffer> _sound_buffers;
+        std::unordered_map<std::string, sf::Music> _songs;
 };
 
 #endif // EXO_RESOURCES_HPP
--- src/Resources.cpp
+++ src/Resources.cpp
@@ -2,7 +2,8 @@
 #include "Resources.hpp"
 
 Resources::Resources() :
-_texture_path("tex/"), _font_path("font/"), _shader_path("shader/") {}
+_texture_path("tex/"), _font_path("font/"), _shader_path("shader/")
+, _sound_path("sound/"), _music_path("music/")  {}
 
 sf::Texture const& Resources::getTexture(std::string const& filename) {
     if (_textures.find(filename) == _textures.end()) {
@@ -16,6 +17,21 @@ sf::Font const& Resources::getFont(std::string const& filename) {
         _fonts[filename].loadFromFile(_texture_path + filename);
     }
     return _fonts.at(filename);
+}
+
+sf::Sound& Resources::getSound(std::string const& filename) {
+    if (_sounds.find(filename) == _sounds.end()) {
+        _sound_buffers[filename].loadFromFile(_sound_path + filename);
+        _sounds[filename].setBuffer(_sound_buffers[filename]);
+    }
+    return _sounds.at(filename);
+}
+
+sf::Music& Resources::getMusic(std::string const& filename) {
+    if (_songs.find(filename) == _songs.end()) {
+        _songs[filename].openFromFile(_music_path + filename);
+    }
+    return _songs.at(filename);
 }
 
 sf::Shader* Resources::allocateShader(std::string const& filename, sf::Shader::Type type) {
